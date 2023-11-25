82c9ebb8196416c2491761958a218168b2490407
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-11-25 03:31:13 +0000

    tidy up

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Command {
@@ -30,7 +29,7 @@ pub enum Command {
 }
 
 // function I used to slowly build up and test
-fn my_transformer(vec_in: &mut Vec<(String, Command)>) {
+pub fn my_transformer(vec_in: &mut Vec<(String, Command)>) {
     // going to mutate the input vector (in place)
     vec_in.iter_mut().for_each(|x: &mut (String, Command)| {
         // grab the String and Command for this particular tuple element
@@ -40,7 +39,7 @@ fn my_transformer(vec_in: &mut Vec<(String, Command)>) {
         match _c {
             Command::Uppercase => x.0 = String::from((x.0).as_str().to_uppercase()),
             Command::Trim => x.0 = String::from((x.0).as_str().trim()),
-            // grab the Command::Appends variant value
+            // unwrap / destructure the Command::Appends variant value
             Command::Append(variant_value) => {
                 let mut app_str: String = String::from("");
                 // append variant_value "bar"s together
@@ -69,49 +68,23 @@ fn main() {
 
     // for each tuple, transform the string (.0) with the command (.1)
     my_transformer(&mut my_vec);
-
-    // assert tests
-    assert_eq!(my_vec[0].0, "HELLO");
-    assert_eq!(my_vec[1].0, "all roads lead to rome!");
-    assert_eq!(my_vec[2].0, "foobar");
-    assert_eq!(my_vec[3].0, "barbarbarbarbarbar");
-
-    // calling official test function just to prevent unused function warnings
-    transformer(&mut my_vec);
 }
 
 mod my_module {
     use super::Command;
+    use crate::my_transformer;
 
     // TODO: Complete the function signature!
     pub fn transformer(vec_in: &mut Vec<(String, Command)>) -> Vec<String> {
-        // going to mutate the input vector (in place)
-        vec_in.iter_mut().for_each(|x: &mut (String, Command)| {
-            // grab the String and Command for this particular tuple element
-            let mut _s: String = (x.0).clone();
-            let _c: Command = (x.1).clone();
-            // perform the Command upon the String
-            match _c {
-                Command::Uppercase => x.0 = String::from((x.0).as_str().to_uppercase()),
-                Command::Trim => x.0 = String::from((x.0).as_str().trim()),
-                // grab the Command::Appends variant value
-                Command::Append(variant_value) => {
-                    let mut app_str: String = String::from("");
-                    // append variant_value "bar"s together
-                    for _ in 0..variant_value {
-                        app_str.push_str("bar");
-                    }
-                    // append these "bar"s onto the original String
-                    (x.0).push_str(&app_str);
-                }
-            };
-        });
 
+        my_transformer(vec_in);
+        
         let mut vec_out: Vec<String> = vec![]; 
         for i in 0..vec_in.len() {
             let _s: String = (vec_in[i].0).to_string();
             vec_out.push(_s);
         }
+
         vec_out
     }
 }
